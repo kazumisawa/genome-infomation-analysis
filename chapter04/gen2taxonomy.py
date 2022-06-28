65021c5662d7861a38674ec21d39f15343e4071b
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2022-06-28 14:09:00 +0900

    Update gen2taxonomy.py

--- chapter04/gen2taxonomy.py
+++ chapter04/gen2taxonomy.py
@@ -8,8 +8,3 @@ with Entrez.efetch(db=database, id=target,
            rettype="gb", retmode="text") as genbank:
     for record in SeqIO.parse( genbank, "genbank") :
         print( record.id, record.__dict__["annotations"]["taxonomy"])
-XP_419622.2 ['Eukaryota', 'Metazoa', 'Chordata', 'Craniata',
-'Vertebrata', 'Euteleostomi', 'Archelosauria', 'Archosauria',
-'Dinosauria', 'Saurischia', 'Theropoda', 'Coelurosauria',
-'Aves', 'Neognathae', 'Galloanserae', 'Galliformes', 'Phasianidae',
-'Phasianinae', 'Gallus']
