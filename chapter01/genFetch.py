d708839bfbf2b04165ec77e8d8b5c4728512cd87
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2022-06-29 13:56:22 +0900

    Update genFetch.py

--- chapter01/genFetch.py
+++ chapter01/genFetch.py
@@ -1,11 +1,8 @@
 import sys
 from Bio import Entrez
-Entrez.email = sys.argv[1]
-database = sys.argv[2]
-target = sys.argv[3]
-with Entrez.efetch(db=database,
-    id=target, rettype="fasta",
-    retmode="text") as genbank:
-    getData = genbank.readlines()
+Entrez.email, database, target = sys.argv[1], sys.argv[2], sys.argv[3]
+with Entrez.efetch(db=database, id=target, rettype="fasta",
+        retmode="text") as gb:
+    getData = gb.readlines()
 for i in getData:
     print(i.rstrip())
