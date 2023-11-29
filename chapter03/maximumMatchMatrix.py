a82e8b20e1d2843f291e5706f7005a21afafbe93
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-11-29 18:13:43 +0900

    Update maximumMatchMatrix.py
    Correct the algorithm (pointed by Dr. Osada)

--- chapter03/maximumMatchMatrix.py
+++ chapter03/maximumMatchMatrix.py
@@ -1,17 +1,28 @@
 import numpy as np
 
 def maximumMatchMatrix(S, d):
-    F0 = np.zeros_like(S)
-    len1, len2 = S.shape
-    add1 = np.array(list(range(len2)))*(-d)-d
-    add2 = np.array(list(range(len1+1)))*(-d)
-    F1 = np.insert(F0, 0, add1, axis=0)
-    F  = np.insert(F1, 0, add2, axis=1)
+    len1, len2 = S.shape #tate, yoko
+    #Fはスコアの行列。一行１列分大きくなる
+    F  = np.zeros( (len1+1, len2+1) )
+    # 左斜め上、上、左を使った時のコードを0, 1, 2とする。
+    upleft, up, left = 0, 1, 2
+    # directionはスコアを計算した時に利用したセルの場所の記録
+    direction  = np.zeros_like(F)
+    # 0列目を-i*dで埋める。
+    for i in range(len1+1):
+        F[i,0], direction[i,0] = -d*i, up 
+    # 0行目を-j*dで埋める。
+    for j in range(len2+1):
+        F[0,j], direction[0,j] = -d*j, left
     compare = np.zeros(3)
     for j in range(len2):
         for i in range(len1):
-            compare[0] = F[i,  j  ] + S[i,j]
-            compare[1] = F[i,  j+1] - d
-            compare[2] = F[i+1,j  ] - d
-            F[i+1,j+1] = np.max(compare)
-    return F
+            compare[ upleft ] = F[i,  j  ] + S[i,j]
+            compare[ up     ] = F[i,  j+1] - d
+            compare[ left   ] = F[i+1,j  ] - d
+            status = np.argmax(compare)
+            print( i+1, j+1 , compare, status )
+            F[i+1,j+1] = compare[ status ]
+            direction[i+1,j+1] = status
+    print(F)
+    return direction
