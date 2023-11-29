377d8b91c4edb3bb36e9354e7b807753c1162193
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-11-29 18:47:45 +0900

    Update needlemanWunsch.py
    Change algorithm

--- chapter03/needlemanWunsch.py
+++ chapter03/needlemanWunsch.py
@@ -4,10 +4,11 @@ import maximumMatchMatrix
 import traceBack
 
 def NeedlemanWunsch70(score, d):
-    F = maximumMatchMatrix.maximumMatchMatrix(score, d)
-    path1, path2 = traceBack.traceBack(F)
+    direction = maximumMatchMatrix.maximumMatchMatrix(score, d)
+    path1, path2 = traceBack.traceBack( direction )
     result1, result2 = list(), list()
     for i in range(len(path1)-1):
         result1.append( path1[i]-1 )
         result2.append( path2[i]-1 )
     return result1, result2
+    
