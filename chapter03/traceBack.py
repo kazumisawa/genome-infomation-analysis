497a60e8437b3c84dcf6f15511dc4fc107f19a91
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-11-29 18:15:34 +0900

    Update traceBack.py
    Correct the algorithm (pointed by Dr. Osada)

--- chapter03/traceBack.py
+++ chapter03/traceBack.py
@@ -1,18 +1,25 @@
 import numpy as np
 
-def traceBack(F): #pathをたどる
-    len1, len2 = F.shape
-    x, y = len1-1, len2-1
+def traceBack( direction ): #pathをたどる
+    len1, len2 = direction.shape # 配列の長さ
+    # スタートの位置
+    x, y = len1 - 1, len2 - 1
+    # 通ったpathを記録  
     path1, path2 = list(), list()
     path1.append( x )
     path2.append( y )
-    while x!=0 and y!=0:
-        s, pos1, pos2 = np.zeros(3), {}, {}
-        s[0], pos1[0], pos2[0] = F[x-1, y-1], x-1, y-1 # gapなし
-        s[1], pos1[1], pos2[1] = F[x-1, y], x-1, y # 配列1の方にgap
-        s[2], pos1[2], pos2[2] = F[x, y-1], x, y-1 # 配列2の方にgap
-        argmax = np.argmax(s)
-        x,y  = pos1[argmax], pos2[argmax]
+    # 左斜め上、上、左を使った時のコードを0, 1, 2とする。
+    upleft, up, left = 0, 1, 2
+    # 行列の右上から左下に向かって埋めていく
+    while x >= 0 and y >= 0:
+        status = direction[x, y] 
+        # statusの値に応じて次のステップを指定
+        if status == upleft:
+            x, y = x - 1, y - 1
+        elif status == up:
+            x = x - 1
+        elif status == left:
+            y = y - 1
         path1.append( x )
         path2.append( y )
     return path1, path2
