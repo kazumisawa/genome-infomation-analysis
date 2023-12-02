6b682066a7a54ffaff39d9be3e202b22707c8a34
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-02 18:17:50 +0900

    Update alignment.py
    Simpified on Dec 02.

--- chapter03/alignment.py
+++ chapter03/alignment.py
@@ -1,16 +1,12 @@
-import sys
-from Bio import SeqIO
+import numpy as np
 import scoreMatrix
-import needlemanWunsch
-import putGap
-import fastaOutput as fa
+import maximumMatchMatrix
+import traceBack
 
-data = list()
-for seq_record in SeqIO.parse(sys.argv[1],"fasta"):
-    data.append(seq_record)
-seq1, seq2  = data[0].seq, data[1].seq
-range1, range2 = ( 0, len(seq1) ), ( 0, len(seq2) )
-S = scoreMatrix.scoreMatrix(seq1, range1, seq2, range2)
-path1, path2 =  needlemanWunsch.NeedlemanWunsch70(S, 1)
-fa.fastaOutput( data[0].description, putGap.putGap( seq1, path1 ) )
-fa.fastaOutput( data[1].description, putGap.putGap( seq2, path2 ) )
+def NeedlemanWunsch70(score, d):
+    direction = maximumMatchMatrix.maximumMatchMatrix(score, d)
+    path1, path2 = traceBack.traceBack( direction )
+    # 先頭に文字$を追加した分を元に戻す
+    result1 = [ path1[i]-1 for i in range( len(path1) -1 ) ]
+    result2 = [ path2[i]-1 for i in range( len(path2) -1 ) ]
+    return result1, result2
