cfcd075d11f1998febe2ebdd0404df650c636746
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-02 18:19:23 +0900

    Update needlemanWunsch.py
    Simplified on Dec 02.

--- chapter03/needlemanWunsch.py
+++ chapter03/needlemanWunsch.py
@@ -6,9 +6,8 @@ import traceBack
 def NeedlemanWunsch70(score, d):
     direction = maximumMatchMatrix.maximumMatchMatrix(score, d)
     path1, path2 = traceBack.traceBack( direction )
-    result1, result2 = list(), list()
-    for i in range(len(path1)-1):
-        result1.append( path1[i]-1 )
-        result2.append( path2[i]-1 )
+    # 先頭に文字$を追加した分を元に戻す
+    result1 = [ path1[i]-1 for i in range( len(path1) -1 ) ]
+    result2 = [ path2[i]-1 for i in range( len(path2) -1 ) ]
     return result1, result2
-    
+
