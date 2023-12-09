8d230933a7c56969f2cfd7859de19d75a585aab2
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-09 17:50:27 +0900

    Update GotohAlignment.py

--- chapter03/GotohAlignment.py
+++ chapter03/GotohAlignment.py
@@ -1,10 +1,10 @@
 import sys
 from Bio import SeqIO
+import score
 import scoreMatrix
-import needlemanWunsch
-import gotoh82
 import putGap
-import fastaOutput as fa
+import affineGapMatchMatrix as am
+import traceBack
 
 data = list()
 for seq_record in SeqIO.parse(sys.argv[1],"fasta"):
@@ -12,6 +12,8 @@ for seq_record in SeqIO.parse(sys.argv[1],"fasta"):
 seq1, seq2  = data[0].seq, data[1].seq
 range1, range2 = ( 0, len(seq1) ), ( 0, len(seq2) )
 S = scoreMatrix.scoreMatrix(seq1, range1, seq2, range2)
-path1, path2 =  gotoh82.gotoh82(S, 0.5, 0.5)
+GOP, GEP = 1.5, 0.5
+direction = am.affineGapMatchMatrix(S, GOP, GEP)
+path1, path2 = traceBack.traceBack( direction )
 fa.fastaOutput( data[0].description, putGap.putGap( seq1, path1 ) )
 fa.fastaOutput( data[1].description, putGap.putGap( seq2, path2 ) )
