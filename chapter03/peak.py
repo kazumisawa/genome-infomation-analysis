38fa4080e1da93b268be4e19f069c966b2d84267
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-12 14:34:18 +0900

    Update peak.py
    If the number of peaks is smaller than length of "corr", use the number of peaks instead of top.

--- chapter03/peak.py
+++ chapter03/peak.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-def peak(corr, top=20):
+def peak(corr, top=20): 
+    # スコアが高い順にピークを返す
+    # ピークの数が多い時は上位20を返す
     dataLen = len(corr)
+    if dataLen < top:
+        top = dataLen #
     corrReal = np.real(corr)
     reverseOrder = np.argsort(corrReal)
     top20 = reverseOrder[:-top-1:-1]
