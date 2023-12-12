eb0cdde8bca3a26699a9a2094a6ca029c1a1bd7e
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-12 14:29:56 +0900

    Update nestedAlignment.py
    Change the algorithm to return strings.

--- chapter03/nestedAlignment.py
+++ chapter03/nestedAlignment.py
@@ -1,13 +1,21 @@
 import scoreMatrix as sc
-import needlemanWunsch as nw
-import gotoh82
+import affineGapMatchMatrix as am
+import putGap
+import traceBack
+#import needlemanWunsch as nw
+#import gotoh82
 def nestedAlignment(seq1, range1, seq2, range2, v, w):
-    path1, path2 = list(), list()
+    #aligned sequences
+    result1, result2 = list(), list()
     for i in range( len(range1) ):
         S = sc.scoreMatrix(seq1, range1[i], seq2, range2[i] )
-        rel1, rel2 =  gotoh82.gotoh82(S, v, w)
-        for position in rel1:
-            path1.append( range1[i][0] + position )
-        for position in rel2:
-            path2.append( range2[i][0] + position )
-    return path1, path2
+        direction = am.affineGapMatchMatrix(S, v, w)
+        path1, path2 = traceBack.traceBack( direction )
+        print(range1)
+        partial1 = seq1[ range1[i][0]:range1[i][1] ]
+        partial2 = seq2[ range2[i][0]:range2[i][1] ]
+        segment1 =  putGap.putGap( partial1, path1 )
+        segment2 =  putGap.putGap( partial2, path2 )
+        result1.append(segment1)
+        result2.append(segment2)
+    return "".join( result1[::-1] ), "".join( result2[::-1] )
