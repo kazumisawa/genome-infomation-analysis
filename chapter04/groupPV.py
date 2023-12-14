8182f2e9aa704f1153267b8fb0e868623831f452
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-14 15:57:06 +0900

    Update groupPV.py
    The groupPV function was modified to allow specifying a region in the arguments.

--- chapter04/groupPV.py
+++ chapter04/groupPV.py
@@ -6,11 +6,13 @@ def norm(org): #平均0, 分散1に
     mean, std = np.mean(org), np.std(org)
     return (org - mean)/std
 
-def groupPV(seqList, p, v):
+def groupPV(seqList, p, v, range):
     plist, vlist = list(), list()
+    start, end = range[0], range[1]
     for seq in seqList:
-        plist.append( cv.convert(seq, p) )
-        vlist.append( cv.convert(seq, v) )
+        partialSeq = seq[start:end]
+        plist.append( cv.convert( partialSeq, p) )
+        vlist.append( cv.convert( partialSeq, v) )
     p = norm( np.mean( np.array(plist), axis=0) )
     v = norm( np.mean( np.array(vlist), axis=0) )
     return (p, v)
