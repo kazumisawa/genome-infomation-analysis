3b655938c936dddcea0e61c483ecfbedfe1cacab
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-14 16:13:04 +0900

    Update additiveMAFFT.py
    Add comments

--- chapter04/additiveMAFFT.py
+++ chapter04/additiveMAFFT.py
@@ -3,7 +3,9 @@ from Bio import SeqIO
 import readVectors
 import groupMAFFT
 import fastaOutput as fa
+import time
 
+start = time.time()
 a,b = readVectors.readVectors(sys.argv[1])
 data = list()
 
@@ -12,13 +14,18 @@ for seq_record in SeqIO.parse(sys.argv[2],"fasta"):
     seq.append( seq_record.seq )
     description.append(seq_record.description)
 
+#アラインメント推定結果のlist
 mainGroup = list()
+#最初は配列0だけを要素に持つ
 mainGroup.append( seq[0] )
 
+#配列追加し、mainGroupのアラインメントを求めて
+#それをmainGroupにする
 for i in range(1, len(seq)):
     subGroup = list()
     subGroup.append(seq[i])
     mainGroup = groupMAFFT.groupMAFFT(mainGroup, subGroup, a, b)
 
+#出力
 for i in range(len(seq)):
     fa.fastaOutput( description[i], mainGroup[i] )
