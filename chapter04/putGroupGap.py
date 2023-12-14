e9858fb3715aee8a16884ddef2017dbf67876329
Kazuharu Misawa <84338100+kazumisawa@users.noreply.github.com>
2023-12-14 16:01:00 +0900

    Update putGroupGap.py
    Modified to specify region to put gaps.

--- chapter04/putGroupGap.py
+++ chapter04/putGroupGap.py
@@ -1,8 +1,9 @@
 import putGap
 
-def putGroupGap(orgGroup, reverse): #pathをたどる
+def putGroupGap(orgGroup, reverse, range1): 
+    # rangeで指定した領域をreverseに従いgapを入れる
+    start, end = range1[0], range1[1]
     result = list()
     for i in range( len(orgGroup) ):
-        result.append( putGap.putGap(orgGroup[i], reverse) )
+        result.append( putGap.putGap(orgGroup[i][start:end], reverse) )
     return result
-
